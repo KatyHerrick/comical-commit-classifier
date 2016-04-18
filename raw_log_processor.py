81df57e06a4f603b2414254a62a2ac5df7a7f742
Katy Herrick <kmh530@nyu.edu>
2016-04-18 12:10:02 -0400

    read in a specified raw_log file

--- raw_log_processor.py
+++ raw_log_processor.py
@@ -1,2 +1,18 @@
 # Given a .txt file such as "fateanother_raw_logs.txt", will output a .txt file
 # of only the commit messages, separated by a newline character.
+
+import argparse
+import os.path
+
+def cwd():
+    return os.path.dirname(os.path.dirname(__file__))
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Cleans unaltered git log output for classification')
+    parser.add_argument('-log', action='store', type=str, help="repo_name_raw_logs.txt", default="data/fateanother_raw_logs.txt")
+
+    args = parser.parse_args()
+
+    with open(cwd() + args.log) as f:
+        print f.read()
