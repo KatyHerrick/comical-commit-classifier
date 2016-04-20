76cef83fcc3ddeb94af78665bc528d5e58414f34
Deniz Parlar and Katy Herrick <dep295+kmh530@nyu.edu>
2016-04-20 13:04:45 -0400

    set up argparsing

--- manual_annotator.py
+++ manual_annotator.py
@@ -1,4 +1,18 @@
 # Given a .txt file such as "fateanother_commits.txt", will prompt the user
 # to mark each commit message as either "funny" or "serious". Outputs a list of
 # tuples of the form:
-# [(“commit message”, “funny”), (“commit message 2”, “funny), …]
+
+import argparse
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-file', action='store', type=str, help="fakerep_commits.txt", \
+        default="data/fakerepo_commits.txt")
+
+    args = parser.parse_args()
+
+    with open(args.file) as f:
+        content = f.readlines()
+        for line in content:
+            print line + "\n"
