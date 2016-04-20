9ff0aeafa83401a29da96fdddc2948652c79a7ed
Deniz Parlar and Katy Herrick <dep295+kmh530@nyu.edu>
2016-04-20 13:43:58 -0400

    rename vars for clarity

--- manual_annotator.py
+++ manual_annotator.py
@@ -1,6 +1,6 @@
 # Given a .txt file such as "fateanother_commits.txt", will prompt the user
 # to mark each commit message as either "funny" or "serious". Outputs a list of
-# tuples of the form:
+# tuples of the form: ('my commit message, 's')
 
 import argparse
 import os.path
@@ -15,17 +15,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-file', action='store', type=str, help="fakerep_commits.txt", \
         default="data/fakerepo_commits.txt")
-
     args = parser.parse_args()
-    answer_key = []
 
     input_file = cwd() + args.file
     output_file = args.file.split('_')[0] + '_answer_key.txt'
-
+    answer_key = []
     with open(input_file, 'r') as f, open(output_file, 'w') as o:
-        content = f.readlines()
+        commits = f.readlines()
         print "Mark commits as either (s)erious or (f)unny"
-        for line in content:
+        for commit in commits:
             sys.stdout.write(line)  # display the next commit to classify
             classification = raw_input()
 
