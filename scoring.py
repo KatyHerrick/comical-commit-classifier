320e7bc819bedd012b6a2148ffe791ae64e2a4f0
ack8 <andrewcklingelhofer@gmail.com>
2016-04-24 22:46:46 -0400

    Fixed indentation

--- scoring.py
+++ scoring.py
@@ -10,16 +10,16 @@ if __name__ == "__main__":
     if sys.argv[1] and sys.argv[2]:
         answer_key = sys.argv[1]
         classifier = sys.argv[2]
-    with open(answer_key, 'r') as a, open(classifier, 'r') as c:
-        answers = a.readlines()
-        classified = c.readlines()
-    if len(answers) == len(classified):
-        for x in range(1, len(answers)):
-            if answers[x-1] == classified[x-1]:
-                correct += 1      
-        print "The classifier identified " + correct + " commits correctly out of " + len(answers) + " total commits."
-        print "The classifier scored " + correct/len(answers + "%."
+        with open(answer_key, 'r') as a, open(classifier, 'r') as c:
+            answers = a.readlines()
+            classified = c.readlines()
+        if len(answers) == len(classified):
+            for x in range(1, len(answers)):
+                if answers[x-1] == classified[x-1]:
+                    correct += 1      
+            print "The classifier identified " + correct + " commits correctly out of " + len(answers) + " total commits."
+            print "The classifier scored " + correct/len(answers + "%."
+        else:
+            print "ERROR: Lengths of answer key and classified commits incorrect."
     else:
-        print "ERROR: Lengths of answer key and classified commits incorrect."
-else:
-    print "ERROR: Must use two arguments (python scoring {path_to_answer_key} {path_to_classifier_key})"
+        print "ERROR: Must use two arguments (python scoring {path_to_answer_key} {path_to_classifier_key})"
