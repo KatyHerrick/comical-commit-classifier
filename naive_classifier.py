6ece23668f60b0a1c420ae53eca38c84b6702810
dparlar <dparlar94@gmail.com>
2016-05-09 18:33:21 -0400

    updated the naive classifier

--- naive_classifier.py
+++ naive_classifier.py
@@ -43,20 +43,56 @@ if __name__ == "__main__":
         classification = ""
         for line in answer_key:
             if line == "serious\n":
-                classification = line
+                classification = "serious"
                 srs_commits.append((msg, classification))
             elif line == "funny\n":
-                classification = line
+                classification = "funny"
                 fun_commits.append((msg, classification))
             else:  # it's a commit message
                 msg = line
+        print len(srs_commits)
+        print len(fun_commits)
 
         commits = []
         for (words, sentiment) in fun_commits + srs_commits:
-            words_normalized = [w.lower() for w in words.split()]
+            words_normalized = [w.lower() for w in words.split() if len(w) >= 3]
             commits.append((words_normalized, sentiment))
 
         word_features = get_word_features(get_words_in_commits(commits))
 
-        training_set = nltk.classify.apply_features(extract_features, commits[0])
+        training_set = nltk.classify.apply_features(extract_features, commits)
 
+        classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+
+
+    test_file = "data/yogstation_commits.txt"
+
+    with open(test_file, 'r') as f:
+        test_file = f.readlines()
+
+        funnies = []
+        not_funnies = []
+        cycle = 0
+        for line in test_file:
+            if cycle == 600:
+                break
+            else:
+                cycle += 1
+            if (classifier.classify(extract_features(line.split())) == "funny"):
+                print line
+                funnies.append(line)
+            else:
+                not_funnies.append(line)
+
+    print ("Number of Serious Commits (out of 600): ")
+    print len(not_funnies)
+    print ("Number of Funny Commits (out of 600): ")
+    print len(funnies)
+
+
+    # print classifier.show_most_informative_features(10)
+
+        # commit = 'and gimmicky as usual. RIP 3 hours If everything goes well Ruler should be ready for release tomorrow'
+        # print classifier.classify(extract_features(commit.split()))
+        # # print extract_features(commit.split())
